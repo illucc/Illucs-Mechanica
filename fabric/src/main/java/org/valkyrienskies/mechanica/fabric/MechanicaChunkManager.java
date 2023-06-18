91f594b27d24056de683c3104f9d6c128d71d599
illuc <h@h>
2023-06-18 14:24:12 +0300

    h

--- fabric/src/main/java/org/valkyrienskies/mechanica/fabric/MechanicaChunkManager.java
+++ fabric/src/main/java/org/valkyrienskies/mechanica/fabric/MechanicaChunkManager.java
@@ -13,52 +13,6 @@ import java.util.*;
 
 import java.util.Map;
 
-public class MechanicaChunkManager extends SavedData {
-    private final HashSet<Map.Entry<UUID, Long>> loadedContraptionChunks;
+public class MechanicaChunkManager {
 
-    public MechanicaChunkManager() {
-        this(new HashSet<>());
-    }
-
-    private MechanicaChunkManager(HashSet<Map.Entry<UUID, Long>> set) {
-        this.loadedContraptionChunks = set;
-    }
-
-    public Set<Map.Entry<UUID, Long>> getChunks() {
-        return loadedContraptionChunks;
-    }
-
-    public static MechanicaChunkManager load(CompoundTag tag) {
-        ListTag chunks = tag.getList("ContraptionChunks", Tag.TAG_COMPOUND);
-        HashSet<Map.Entry<UUID, Long>> set = new HashSet<>();
-        if(chunks != null) {
-            chunks.forEach(subTag -> {
-                CompoundTag forcedChunk = (CompoundTag)subTag;
-                set.add(Map.entry(forcedChunk.getUUID("UUID"), forcedChunk.getLong("ChunkPos")));
-            });
-        }
-        return new MechanicaChunkManager(set);
-    }
-
-    public void forceChunk(UUID uuid, ChunkPos chunkPos, boolean forced) {
-        long l = chunkPos.toLong();
-        if(forced)
-            loadedContraptionChunks.add(Map.entry(uuid, l));
-        else {
-            loadedContraptionChunks.removeIf(entry -> (entry.getKey().equals(uuid) && entry.getValue() == l));
-        }
-    }
-
-    @Override
-    public CompoundTag save(CompoundTag compoundTag) {
-        ListTag tag = new ListTag();
-        loadedContraptionChunks.forEach(entry -> {
-            CompoundTag subTag = new CompoundTag();
-            subTag.putUUID("UUID", entry.getKey());
-            subTag.putLong("ChunkPos", entry.getValue());
-            tag.add(subTag);
-        });
-        compoundTag.put("ContraptionChunks", tag);
-        return compoundTag;
-    }
 }
